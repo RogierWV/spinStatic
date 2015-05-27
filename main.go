a0b6f795a59ea054fb41233318b5f9c9ccdc81ee
Rogier <rogiervelting@gmail.com>
2015-05-27 09:34:16 +0200

    slimmed deps

--- main.go
+++ main.go
@@ -1,28 +1,3 @@
-/*package main
-
-import (
-	"github.com/julienschmidt/httprouter"
-	"net/http"
-	"log"
-	"os"
-	"fmt"
-)
-
-func Static(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	http.ServeFile(w,r,"./static/"+ps.ByName("file"))
-}
-
-func main() {
-	router := httprouter.New()
-	router.GET("/*file", Static)
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-	fmt.Printf("Starting server at localhost:%s...", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
-}*/
-
 package main
 
 import (
