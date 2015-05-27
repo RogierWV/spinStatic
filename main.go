67639a9e9e12a269b91a14d85dd8d4e0c8b5ce67
Rogier <rogiervelting@gmail.com>
2015-05-27 09:37:24 +0200

    Slimmed down code further

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
-	log.Printf("Listening on port %s...", port)
-	http.ListenAndServe(":"+port, nil)
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
