8dc38f0150f0de895ccb7f5a24c549ba11be021a
Rogier <rogiervelting@gmail.com>
2015-06-12 14:01:28 +0200

    added in memory caching

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"net/http"
 	"os"
+	"log"
+	"github.com/ranveerkunal/memfs"
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("static")))
+	fs, err := memfs.New("./static/")
+	if err != nil {
+		log.Fatalf("Failed to create memfs: %s err: %v", "./static/", err)
+	}
+
+	http.Handle("/", http.FileServer(fs))
 	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
